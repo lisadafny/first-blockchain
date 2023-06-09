bdde71f16e8662a419f9ce49d11ef4135e658bf1
Lisa Dafny <lisadafny.tech@gmail.com>
2023-06-08 22:31:42 -0300

    create new transaction

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,24 +1,38 @@
 function Blockchain() {
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransaction = [];
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransaction,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash
     };
 
-    this.newTransactions = [];
+    this.pendingTransaction = [];
     this.chain.push(newBlock);
 
     return newBlock;
 }
 
+Blockchain.prototype.getLastBlock = function(){
+    return this.chain[this.chain.length -1];
+}
 
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient
+    };
+
+    this.pendingTransaction.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
+}
 
 export default Blockchain;
